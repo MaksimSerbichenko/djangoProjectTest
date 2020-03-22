0fd3bf46a04f0c5922c1a87867dc12c72f0d8af4
MaksimSerbichenko <mserbichenko@gmail.com>
2020-03-22 11:14:06 +0200

    1_7_2_f_lesson

--- landing/urls.py
+++ landing/urls.py
@@ -1,8 +1,12 @@
 from django.conf.urls import url
+from django.conf.urls.static import static
 
+from djangoProjectTest import settings
 from landing import views
 
 urlpatterns = [
     url(r'^', views.home, name='home'),
     url(r'^landing/', views.landing, name='landing')
 ]
+
+
--- landing/views.py
+++ landing/views.py
@@ -1,16 +1,16 @@
 from django.http import HttpResponseRedirect
 from django.shortcuts import render
-import datetime
-import time
-from .forms import SabscribersForm
+from django.views import generic
+
 from products.models import *
+from .forms import SabscribersForm
 
 
 def landing(request):
     name = "CoderMax"
-    #current_day = "22.02.2020"
-    #current_day = datetime.datetime.today()
-    #print(current_day.strftime("%m/%d/%Y"))
+    # current_day = "22.02.2020"
+    # current_day = datetime.datetime.today()
+    # print(current_day.strftime("%m/%d/%Y"))
     form = SabscribersForm(request.POST or None)
 
     if request.method == "POST" and form.is_valid():
@@ -26,3 +26,6 @@ def landing(request):
 def home(request):
     products_images = ProductImage.objects.filter(is_active=True, is_main=True)
     return render(request, 'landing/home.html', locals())
+
+
+
